483455b2ac38b3d3ade7137979c2ff0962a21d35
ubuntu <ubuntu@ubuntu-VirtualBox.(none)>
2015-03-10 01:00:40 -0400

    gains

--- src/main.cc
+++ src/main.cc
@@ -6,6 +6,25 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <iostream>
+#include <fstream>
 
 
 using namespace std;
+
+
+int main()
+{
+
+	
+	
+	ifstream file ( "/home/ubuntu/Desktop/BasketbalStat/leagues_NBA_2014_totals_totals.csv" );
+	string value;
+	cout << "dgad" << endl;
+	while ( file.good() )
+	{
+     		getline ( file, value, ',' ); // read a string until next comma: http://www.cplusplus.com/reference/string/getline/
+     		cout << string( value, 1, value.length()-2 ); // display value removing the first and the last character from it
+		cout << "we here" << endl;
+	}
+	cout << "a" << endl;
+}
